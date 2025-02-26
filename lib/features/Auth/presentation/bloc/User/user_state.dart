c5f5087248e259a52dab83c83c62722c7f47ed8c
Nestor Gonzalez <nestorgt37@gmail.com>
2025-02-25 20:49:02 -0600

    SingUp structure started

--- lib/features/Auth/presentation/bloc/User/user_state.dart
+++ lib/features/Auth/presentation/bloc/User/user_state.dart
@@ -2,9 +2,22 @@ part of 'user_bloc.dart';
 
 sealed class UserState extends Equatable {
   const UserState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class UserInitial extends UserState {}
+
+final class SignUpLoading extends UserState {}
+
+final class SignUpSuccess extends UserState {}
+
+final class SignUpFailure extends UserState {
+  final String errorMessage;
+
+  const SignUpFailure(this.errorMessage);
+
+  @override
+  List<Object> get props => [errorMessage];
+}
