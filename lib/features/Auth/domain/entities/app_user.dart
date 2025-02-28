e23e851cc5ce0da3e2a8d3455367e711795f6edd
Nestor Gonzalez <nestorgt37@gmail.com>
2025-02-27 21:02:37 -0600

    User entitie added

---
+++ lib/features/Auth/domain/entities/app_user.dart
@@ -0,0 +1,24 @@
+class AppUser {
+  final String Id;
+  final String UserName;
+  final String AccessToken;
+  final int AuthTokenValidityInMins;
+  final String RefreshToken;
+  final int RefreshTokenValidityInDays;
+  final String? UserImageUrl;
+  final int? CityId;
+  final int? TimeZoneId;
+  final int? LanguageId;
+
+  AppUser(
+      {required this.Id,
+      required this.UserName,
+      required this.AccessToken,
+      required this.AuthTokenValidityInMins,
+      required this.RefreshToken,
+      required this.RefreshTokenValidityInDays,
+      required this.UserImageUrl,
+      required this.CityId,
+      required this.TimeZoneId,
+      required this.LanguageId});
+}
