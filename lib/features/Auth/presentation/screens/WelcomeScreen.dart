f86e81a9b8fdb56cb83e1620adc351959f3b0c5f
Nestor Gonzalez <nestorgt37@gmail.com>
2025-03-08 20:52:38 -0600

    Login provider test maded

--- lib/features/Auth/presentation/screens/WelcomeScreen.dart
+++ lib/features/Auth/presentation/screens/WelcomeScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:georutasmovil/features/Auth/presentation/screens/SignInScreen.dart';
 import 'package:georutasmovil/features/Auth/presentation/screens/SignUpScreen.dart';
 import 'package:georutasmovil/features/Auth/presentation/widgets/CustomScaffold.dart';
--- lib/features/Auth/presentation/widgets/CustomScaffold.dart
+++ lib/features/Auth/presentation/widgets/CustomScaffold.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:georutasmovil/features/Auth/presentation/bloc/User/user_bloc.dart';
 
 class CustomScaffold extends StatelessWidget {
   final Widget? child;
@@ -16,27 +13,18 @@ class CustomScaffold extends StatelessWidget {
           elevation: 0,
         ),
         extendBodyBehindAppBar: true,
-        body: BlocBuilder<UserBloc, UserState>(
-          builder: (contex, state) {
-            switch (state) {
-              case UserInitial():
-                return Stack(
-                  children: [
-                    Image.asset(
-                      "assets/images/backgroundScreen.png",
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                    SafeArea(
-                      child: child!,
-                    )
-                  ],
-                );
-              default:
-                return const Text("Unkwoun state");
-            }
-          },
+        body: Stack(
+          children: [
+            Image.asset(
+              "assets/images/backgroundScreen.png",
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            SafeArea(
+              child: child!,
+            )
+          ],
         ));
   }
 }
