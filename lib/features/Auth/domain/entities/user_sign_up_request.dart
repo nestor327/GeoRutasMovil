00890f97b35aa06ac35008f075b45da4d78075ae
Nestor Gonzalez <nestorgt37@gmail.com>
2025-03-21 21:42:38 -0600

    SingUpRequest updated

--- lib/features/Auth/domain/entities/user_sign_up_request.dart
+++ lib/features/Auth/domain/entities/user_sign_up_request.dart
@@ -1,11 +1,11 @@
 class UserSignUpRequest {
   final String Email;
   final String Password;
-  final int FirstName;
+  final String FirstName;
   final String LastName;
   final int CountryId;
-  final String? TimeZoneId;
-  final int? UserImageUrl;
+  final int TimeZoneId;
+  final String? UserImageUrl;
   final int? LanguageId;
 
   UserSignUpRequest(
