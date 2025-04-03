f03843c35781f6b0ba4fd6f53ce6ea8d0dc1b410
Nestor Gonzalez <nestorgt37@gmail.com>
2025-04-02 21:11:07 -0600

    Endpoint and entitie updated

--- lib/features/Routes/data/models/bus_type_model.dart
+++ lib/features/Routes/data/models/bus_type_model.dart
@@ -3,4 +3,21 @@ import 'package:georutasmovil/features/Routes/domain/entities/bus_type.dart';
 class BusTypeModel extends BusType {
   BusTypeModel(
       {required super.Id, required super.Name, required super.Description});
+
+  factory BusTypeModel.fromJson(json) {
+    return BusTypeModel(
+        Description: json["description"], Id: json["id"], Name: json["name"]);
+  }
+
+  static List<BusTypeModel> parseEntidades(dynamic jsonData) {
+    if (jsonData is List) {
+      // Si es una lista, parseamos cada elemento a Entidad
+      return jsonData.map((item) => BusTypeModel.fromJson(item)).toList();
+    } else if (jsonData is Map<String, dynamic>) {
+      // Si es un solo objeto, lo convertimos en una lista con un solo elemento
+      return [BusTypeModel.fromJson(jsonData)];
+    } else {
+      throw Exception("Formato de JSON no reconocido");
+    }
+  }
 }
