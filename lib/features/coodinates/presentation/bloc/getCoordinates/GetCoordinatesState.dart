1570d04c2916ec24f19a0eab4bb97b4d56bbffb6
Nestor Gonzalez <nestorgt37@gmail.com>
2025-04-05 21:07:37 -0600

    Trip model added

--- lib/features/coodinates/presentation/bloc/getCoordinates/GetCoordinatesState.dart
+++ lib/features/coodinates/presentation/bloc/getCoordinates/GetCoordinatesState.dart
@@ -1,28 +1,30 @@
 import 'package:georutasmovil/core/error/Failure.dart';
-import 'package:georutasmovil/features/coodinates/domain/entities/coordinate.dart';
+import 'package:georutasmovil/features/coodinates/domain/entities/Coordinate.dart';
 
-sealed class GetCoordinatesState {}
+sealed class GetCoordinateLocalsState {}
 
-final class GetcoordinatesInitial extends GetCoordinatesState {}
+final class GetCoordinateLocalsInitial extends GetCoordinateLocalsState {}
 
-final class GetcoordinatesLoading extends GetCoordinatesState {}
+final class GetCoordinateLocalsLoading extends GetCoordinateLocalsState {}
 
-final class GetcoordinatesSuccess extends GetCoordinatesState {
-  final List<Coordinate> list;
-  GetcoordinatesSuccess({required this.list});
+final class GetCoordinateLocalsSuccess extends GetCoordinateLocalsState {
+  final List<CoordinateLocal> list;
+  GetCoordinateLocalsSuccess({required this.list});
 }
 
-final class GetCoordinatesBetweenTwoPointsState extends GetCoordinatesState {
-  final List<Coordinate> list;
-  GetCoordinatesBetweenTwoPointsState({required this.list});
+final class GetCoordinateLocalsBetweenTwoPointsState
+    extends GetCoordinateLocalsState {
+  final List<CoordinateLocal> list;
+  GetCoordinateLocalsBetweenTwoPointsState({required this.list});
 }
 
-final class GetCoordinatesBetweenTwoBusStopsState extends GetCoordinatesState {
-  final List<Coordinate> list;
-  GetCoordinatesBetweenTwoBusStopsState({required this.list});
+final class GetCoordinateLocalsBetweenTwoBusStopsState
+    extends GetCoordinateLocalsState {
+  final List<CoordinateLocal> list;
+  GetCoordinateLocalsBetweenTwoBusStopsState({required this.list});
 }
 
-final class GetCoordinatesFailure extends GetCoordinatesState {
+final class GetCoordinateLocalsFailure extends GetCoordinateLocalsState {
   final Failure failure;
-  GetCoordinatesFailure({required this.failure});
+  GetCoordinateLocalsFailure({required this.failure});
 }
