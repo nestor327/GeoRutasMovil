7d990bffab6a8f449b9c9d211e02ded0d115e2f7
Nestor Gonzalez <nestorgt37@gmail.com>
2025-04-08 21:32:18 -0600

    apykey enviroment variable register and used

--- lib/shared/utils/env.dart
+++ lib/shared/utils/env.dart
@@ -1,4 +1,5 @@
 abstract class EnvConfig {
   static String baseUrl = const String.fromEnvironment("BASEURL");
   static String mapApyKey = const String.fromEnvironment("MAPKEY");
+  static String geoRutasApyKey = const String.fromEnvironment("GEORUTASAPYKEY");
 }
