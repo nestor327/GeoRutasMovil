83f5870ba6ad4782509bae6fa9ce74c735870e7b
Nestor Gonzalez <nestorgt37@gmail.com>
2025-04-22 21:16:20 -0600

    Datasource google place created

---
+++ lib/features/Routes/data/models/google_auto_complete_response_model.dart
@@ -0,0 +1,5 @@
+import 'package:georutasmovil/features/Routes/domain/entities/google_auto_complete_response.dart';
+
+class GoogleAutoCompleteResponseModel extends GoogleAutoCompleteResponse {
+  GoogleAutoCompleteResponseModel({required super.status});
+}
