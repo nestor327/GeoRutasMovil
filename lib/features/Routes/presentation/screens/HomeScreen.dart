dac087b909bcbbe67fa3ea7a90095a03614cdb45
Nestor Gonzalez <nestorgt37@gmail.com>
2025-05-07 21:13:05 -0600

    Coordenates added to markers

--- lib/features/Routes/presentation/screens/HomeScreen.dart
+++ lib/features/Routes/presentation/screens/HomeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:georutasmovil/features/Routes/domain/entities/get_bus_by_type_request.dart';
+import 'package:georutasmovil/features/Routes/presentation/bloc/routelocations/route_locations_bloc.dart';
 import 'package:georutasmovil/features/Routes/presentation/bloc/routes/route_bloc.dart';
 import 'package:georutasmovil/features/Routes/presentation/widgets/BusMenu.dart';
 import 'package:georutasmovil/features/Routes/presentation/widgets/search_panel.dart';
@@ -23,87 +24,104 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     // var _showCoordinates = false;
 
-    return SafeArea(
-      child: BlocListener<RouteBloc, RouteState>(
-        listener: (context, state) {
-          if (state is GetCoordinateRouteByScheduleIdSuccess) {
-            setState(() {
-              _polylineCoordinates = state.response.map((toElement) {
-                return LatLng(toElement.Latitude, toElement.Longitude);
-              }).toList();
-            });
-          }
-        },
-        child: Scaffold(
-          appBar: AppBar(centerTitle: true, title: const Text("Ciudad")),
-          body: Stack(
-            children: [
-              GoogleMap(
-                initialCameraPosition:
-                    CameraPosition(target: cityCoordinates, zoom: 16),
-                markers: {
-                  Marker(
-                    markerId: MarkerId('marker_1'),
-                    position: LatLng(12.729415, -86.126544),
-                    infoWindow: InfoWindow(title: 'Punto A', snippet: 'Inicio'),
-                  ),
-                  Marker(
-                    markerId: MarkerId('marker_2'),
-                    position: LatLng(12.742475, -86.121136),
-                    infoWindow:
-                        InfoWindow(title: 'Punto B', snippet: 'Destino'),
-                  ),
-                },
-                polylines: {
-                  if (_polylineCoordinates.length > 0)
-                    Polyline(
-                        polylineId: PolylineId("Poly1"),
-                        color: Colors.blue,
-                        points: _polylineCoordinates,
-                        width: 4),
-                  if (_polylineCoordinates.length == 0)
-                    const Polyline(
-                        polylineId: PolylineId("Poly2"),
-                        color: Colors.blue,
-                        points: [
-                          LatLng(12.742475, -86.121136),
-                          LatLng(12.729415, -86.126544)
-                        ],
-                        width: 4)
-                },
-                key: Key(EnvConfig.mapApyKey),
-              ),
-              if (_showSearchPanel) SearchPanel(),
-            ],
+    return BlocBuilder<RouteLocationBloc, RouteLocationState>(
+        builder: (contex, state) {
+      Set<Marker> markers = {};
+
+      if (state.originLat != null && state.originLng != null) {
+        markers.add(
+          Marker(
+            markerId: MarkerId('origin'),
+            position: LatLng(state.originLat!, state.originLng!),
+            infoWindow: InfoWindow(title: 'Origen'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueGreen),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: (index) {
-              GetBusesByTypeRequest request =
-                  new GetBusesByTypeRequest(BusTypeId: 1);
+        );
+      }
 
-              context
-                  .read<RouteBloc>()
-                  .add(GetBusesByTypeEvent(request: request));
+      if (state.destinationLat != null && state.destinationLng != null) {
+        markers.add(
+          Marker(
+            markerId: MarkerId('destination'),
+            position: LatLng(state.destinationLat!, state.destinationLng!),
+            infoWindow: InfoWindow(title: 'Destino'),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          ),
+        );
+      }
 
-              if (index == 0) {
-                ShowBusMenu(context);
-              } else if (index == 1) {
-                setState(() {
-                  _showSearchPanel = !_showSearchPanel;
-                });
-              }
-            },
-            items: const [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.directions_bus_rounded), label: "Buses"),
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.supervised_user_circle_rounded),
-                  label: "Settings"),
-            ],
+      return SafeArea(
+        child: BlocListener<RouteBloc, RouteState>(
+          listener: (context, state) {
+            if (state is GetCoordinateRouteByScheduleIdSuccess) {
+              setState(() {
+                _polylineCoordinates = state.response.map((toElement) {
+                  return LatLng(toElement.Latitude, toElement.Longitude);
+                }).toList();
+              });
+            }
+          },
+          child: Scaffold(
+            appBar: AppBar(centerTitle: true, title: const Text("Ciudad")),
+            body: Stack(
+              children: [
+                GoogleMap(
+                  initialCameraPosition:
+                      CameraPosition(target: cityCoordinates, zoom: 16),
+                  markers: markers,
+                  polylines: {
+                    if (_polylineCoordinates.length > 0)
+                      Polyline(
+                          polylineId: PolylineId("Poly1"),
+                          color: Colors.blue,
+                          points: _polylineCoordinates,
+                          width: 4),
+                    if (_polylineCoordinates.length == 0)
+                      const Polyline(
+                          polylineId: PolylineId("Poly2"),
+                          color: Colors.blue,
+                          points: [
+                            LatLng(12.742475, -86.121136),
+                            LatLng(12.729415, -86.126544)
+                          ],
+                          width: 4)
+                  },
+                  key: Key(EnvConfig.mapApyKey),
+                ),
+                if (_showSearchPanel) SearchPanel(),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                GetBusesByTypeRequest request =
+                    new GetBusesByTypeRequest(BusTypeId: 1);
+
+                context
+                    .read<RouteBloc>()
+                    .add(GetBusesByTypeEvent(request: request));
+
+                if (index == 0) {
+                  ShowBusMenu(context);
+                } else if (index == 1) {
+                  setState(() {
+                    _showSearchPanel = !_showSearchPanel;
+                  });
+                }
+              },
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.directions_bus_rounded), label: "Buses"),
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.supervised_user_circle_rounded),
+                    label: "Settings"),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
