b97f8d43316c6ffb04978448a1c1e5126201a3e8
Nestor Gonzalez <nestorgt37@gmail.com>
2025-05-14 21:01:50 -0600

    Async function implemented

--- lib/features/Routes/presentation/bloc/buses/buses_bloc.dart
+++ lib/features/Routes/presentation/bloc/buses/buses_bloc.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:georutasmovil/features/Routes/domain/entities/bus.dart';
+import 'package:georutasmovil/shared/utils/env.dart';
 import 'buses_event.dart';
 import 'buses_state.dart';
 
@@ -15,12 +17,35 @@ class BusesBloc extends Bloc<BusesEvent, BusesState> {
     try {
       // Simulación de carga
       await Future.delayed(Duration(seconds: 2));
+      final Dio dio = Dio();
 
       // Puedes reemplazar esto con una llamada real a una API o base de datos
 
       if (event.tipoBusqueda.isEmpty) {
         print("No hay ni mierda que validar");
       } else {
+        if (event.tipoBusqueda == "Cooperativas") {
+          final resp = await dio.get(
+            'http://192.168.1.14:5005/v1/bus/buses-by-bustype?busTypeId=${1}',
+            options: Options(
+              headers: {
+                'accept': 'text/plain',
+                'X-Language': 'es',
+                'Content-Type': 'application/json',
+                'X-Api-Key': EnvConfig.geoRutasApyKey
+              },
+            ),
+          );
+
+          print("El resp code fue: ${resp.statusCode}");
+          if (resp.statusCode == 200) {
+            final List<Bus> busTypeModels = Bus.parseEntidades(resp.data);
+
+            emit(BusesLoaded(
+                tipoBusqueda: event.tipoBusqueda, buses: busTypeModels));
+          }
+        }
+
         print("El tipo de busqueda fue ${event.tipoBusqueda}");
       }
 
