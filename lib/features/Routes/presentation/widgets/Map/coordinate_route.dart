fbdb3542c88ea40223ad6cbada13ef137a8d8b68
Nestor Gonzalez <105031333+nestor327@users.noreply.github.com>
2025-06-02 21:56:40 -0600

    Coordinate route file added

---
+++ lib/features/Routes/presentation/widgets/Map/coordinate_route.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:georutasmovil/features/Routes/domain/entities/get_coordinates_between_stops_request.dart';
+import 'package:georutasmovil/features/Routes/domain/entities/trip.dart';
+import 'package:georutasmovil/features/Routes/domain/entities/trip_paginated.dart';
+import 'package:georutasmovil/features/Routes/presentation/bloc/routes/route_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CoordinateRoute extends StatefulWidget {
+  const CoordinateRoute({super.key});
+
+  @override
+  State<CoordinateRoute> createState() => _CoordinateRouteState();
+}
+
+class _CoordinateRouteState extends State<CoordinateRoute> {
+  List<Trip> viajes = [];
+  List<Marker> marcadores = [];
+  List<Polyline> routesPartials = [];
+  TripPaginated tripPaginated = new TripPaginated(
+      PagingInfo: PagingInf(PerPage: 0, CurrentPage: 0, TotalRecords: 0),
+      Records: List.empty());
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocListener(listeners: [
+      BlocListener<RouteBloc, RouteState>(listener: (context, state) {
+        if (state is GetTripsByLocationSuccess) {
+          //Obtener la lista de viajes
+          tripPaginated = state.response;
+          viajes = tripPaginated.Records;
+
+          //Hay que actualizar el endpoint para que reciba un conjunto de intervalos de paradas
+
+          context.read<RouteBloc>().add(GetCoordinatesBetweenStopsEvent(
+              request: GetCoordinatesBetweenStopsRequest(
+                  FromStopId: viajes[0].Trips[0].FromStopId,
+                  ToStopId: viajes[0].Trips[0].ToStopId)));
+        }
+      })
+    ], child: const Placeholder());
+  }
+}
